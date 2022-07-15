49dbde157f483fdd3b04c2cc993ece551d790f1e
gygy2022 <2022gygy@gmail.com>
2022-07-16 01:38:36 +0900

    이미지 이름 담는 textarea 주석 처리

--- src/pages/CreateLive.js
+++ src/pages/CreateLive.js
@@ -177,7 +177,7 @@ function CreateLive() {
           <UploadImagePreview previewImg={previewImg}></UploadImagePreview>
           <label className="secondary upload-label-button" for="upload-image">이미지 업로드</label>
           <UploadImageNameWrap className="upload-image-name-wrap" textHeight={textHeight}>
-            <textarea id="upload-image-name" row="1" value={imgName} ref={imgName_ref} spellcheck="false" readOnly></textarea>
+            {/* <textarea id="upload-image-name" row="1" value={imgName} ref={imgName_ref} spellcheck="false" readOnly></textarea> */}
 
 
             </UploadImageNameWrap>
